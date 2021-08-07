bd79c09a3b89049cd749192c668bda8b13a624d2
rsumitaj <sumitrajtech@gmail.com>
2021-08-07 15:20:48 +0530

    add state(obj to store data)

--- src/cartItem.js
+++ src/cartItem.js
@@ -1,7 +1,17 @@
 import React from 'react' ;
 
 class CartItem extends React.Component {
+  constructor (){
+    super();
+    this.state = {
+      title:"Apple Iphone",
+      price:99999,
+      Qty:1,
+      img:' '
+    }
+  }
   render(){
+    const {title,price,Qty}=this.state;
     return (
       <div className="cart-item">
 
@@ -10,12 +20,14 @@ class CartItem extends React.Component {
         </div>
 
         <div className="right-block">
-          <div style={{fontSize:25}}>Phone</div>
-          <div style={{color:'#777'}}>Rs 9999</div>
-          <div style={{color:'#777'}}>Qty : 1</div>
+          <div style={{fontSize:25}}>{title}</div>
+          <div style={{color:'#777'}}>Rs {price}</div>
+          <div style={{color:'#777'}}>Qty : {Qty}</div>
           <div className="cart-item-actions">
             {/*  buttons */}
-
+            <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/512/1828/1828926.png" />
+            <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
+            <img alt="delete" className="action-icons" src="https://img-premium.flaticon.com/png/512/3368/premium/3368158.png?token=exp=1628330026~hmac=27d8d700647101fe0bfb574f21d287ee" />
           </div>
         </div>
 
