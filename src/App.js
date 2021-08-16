e80d28b1a7d7a1dee5af8d7f43a3af9de0f85cfd
rsumitaj <sumitrajtech@gmail.com>
2021-08-16 20:07:49 +0530

    get data  from database

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
 import React from 'react';
 import Cart from './cart';
 import Navbar from './Navbar'
+import  firebase from 'firebase';
 
 class App extends React.Component {
 
   constructor (){
     super();
     this.state = {
-      products:[
-        {
-          title:"Macbook Air M1",
-          price:79999,
-          Qty:5,
-          img:'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/macbook-air-space-gray-select-201810?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1603332211000',
-          id:1
-        },
-        {
-          title:"Apple Iphone",
-          price:89999,
-          Qty:2,
-          img:'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-iphone-xs-silver?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1579299535944',
-          id:2
-        },
-        {
-          title:"Apple Watch",
-          price:19999,
-          Qty:8,
-          img:'https://m.media-amazon.com/images/I/71fwbMm1NBL._AC_SL1500_.jpg',
-          id:3
-        }
-      ]
+      products:[],
+      loading:true
     }
     //this.increaseQuantity = this.increaseQuantity.bind(this) ;
   }
+
+  componentDidMount(){
+    // firebase
+    //     .firestore()
+    //     .collection('products')
+    //     .get()
+    //     .then((snapshot)=>{
+    //       snapshot.docs.map((doc)=>{
+    //         console.log(doc.data());
+    //       });
+
+    //       const products = snapshot.docs.map((doc)=>{
+    //         const data = doc.data();
+    //         data['id'] = doc.id;
+    //         return data;
+    //       })
+
+    //       this.setState({
+    //         products
+    //       })
+
+    //     });
+
+    firebase
+        .firestore()
+        .collection('products')
+        .onSnapshot((snapshot)=>{
+          snapshot.docs.map((doc)=>{
+            console.log(doc.data());
+          });
+
+          const products = snapshot.docs.map((doc)=>{
+            const data = doc.data();
+            data['id'] = doc.id;
+            return data;
+          })
+
+          this.setState({
+            products
+          })
+
+        });
+
+  }
+
   handleIncreaseQuantity = (product) => {
     const {products}=this.state;
     const index = products.indexOf(product);
@@ -65,7 +90,8 @@ class App extends React.Component {
     const items = products.filter((item) => item.id!== id);
 
     this.setState({
-      products:items
+      products:items,
+      loading:false
     });
   }
 
@@ -94,7 +120,7 @@ class App extends React.Component {
   }
 
   render(){
-    const {products}=this.state;
+    const {products,loading}=this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
@@ -106,6 +132,7 @@ class App extends React.Component {
               onDeleteProduct = {this.handleDeleteProduct}
         />
 
+        {loading && <h1>Loading Products...</h1>}
         <div style={ {fontSize:20,padding:20} }>
           TOTAL: Rs.{this.getCartTotal()}
         </div>
